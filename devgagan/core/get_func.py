e83214085b5c68bd91aabcb3dd3f6441d6f208b9
rocky2109 <rockys.2109rs@gmail.com>
2025-07-07 12:25:06 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -828,7 +828,7 @@ async def callback_query_handler(event):
         sessions[user_id] = 'setcaption'
 
     elif data == 'setreplacement':
-        await event.respond("🔄 Send replacement inside this ''\n\n'oldword' 'newword'\n\n")
+        await event.respond("🔄 Send replacement inside this ' '\n\n'oldword' 'newword'\n\n")
         sessions[user_id] = 'setreplacement'
 
     elif data == 'addsession':
@@ -980,7 +980,7 @@ async def handle_user_input(event):
                     replacements = load_replacement_words(user_id)
                     replacements[word] = replace_word
                     save_replacement_words(user_id, replacements)
-                    await event.respond(f"⇆ Replacement saved: 🌚 '{word}' ≫ '{replace_word}' 🌝")
+                    await event.respond(f"⇆ Replacement saved:\n\n 🌚 '{word}' ≫ '{replace_word}' 🌝")
 
         elif session_type == 'addsession':
             session_string = event.text
